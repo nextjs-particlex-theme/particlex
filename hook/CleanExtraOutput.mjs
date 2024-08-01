4944e83ab685d809d64d8a8a438716490017ef1e
icebing <xu2237803016@outlook.com>
2024-08-01 14:04:55 +0800

    chore: 如果配置了 CDN，则在打包完成后删除多余的静态资源

---
+++ hook/CleanExtraOutput.mjs
@@ -0,0 +1,11 @@
+/**
+ * 如果配置了 CDN，则删除多余的静态资源.
+ * 由于 next.js 限制，在 webpack hook 的 done 阶段删除不了 out 目录中的内容，所以只能在这里删除...
+ */
+import fs from "node:fs"
+
+if (!process.env.NEXT_PUBLIC_CND_PUBLIC_PATH_BASE_URL || !fs.statSync('out').isDirectory()) {
+  return
+}
+
+fs.rmSync('out', { recursive: true, force: true })
