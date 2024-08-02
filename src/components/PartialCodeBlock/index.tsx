c4cb01eb5dd9e9e9b4c33ea7bbaf7595655ad6d6
icebing <xu2237803016@outlook.com>
2024-08-02 14:40:56 +0800

    feat: 复制时添加动画提示复制成功

--- src/components/PartialCodeBlock/index.tsx
+++ src/components/PartialCodeBlock/index.tsx
@@ -2,41 +2,19 @@
 import React, { useEffect, useRef, useState } from 'react'
 import style from './post-content.module.scss'
 import { Icons } from '@/app/svg-symbols'
+import { concatClassName } from '@/lib/DomUtils'
 
 interface PartialCodeBlockProps {
   content: string
   lang: string
 }
 
-
-interface RichContentProps extends PartialCodeBlockProps {
-  wrapLine: boolean
-  onRequirePartialHide: () => void
-}
-
-const _RichContent: React.FC<RichContentProps> = props => {
-  const codeContainer = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    if (!codeContainer.current) {
-      return
-    }
-    if (codeContainer.current.clientHeight >= window.innerHeight / 2) {
-      props.onRequirePartialHide()
-    }
-  }, [props])
-  
-  return (
-    <div className={style.codeContent} ref={codeContainer} style={props.wrapLine ? { textWrap: 'wrap' } : undefined} dangerouslySetInnerHTML={{ __html: props.content }}/>
-  )
-}
-
-const RichContent = React.memo(_RichContent)
-
 const _PartialCodeBlock: React.FC<PartialCodeBlockProps> = props => {
   const [wrapLineActive, setWrapLineActive] = React.useState(false)
   const [copyIconClass, setCopyIconClass] = React.useState(style.icon)
   const [partialHide, setPartialHide] = useState(false)
+  const codeContainer = useRef<HTMLDivElement>(null)
+  const [copyAniClass, setCopyAniClass] = React.useState(style.copyAniBegin)
 
   const onWrapLineClick = () => {
     setWrapLineActive(!wrapLineActive)
@@ -50,23 +28,41 @@ const _PartialCodeBlock: React.FC<PartialCodeBlockProps> = props => {
     navigator.clipboard.writeText(props.content).catch(e => {
       alert(e.message)
     })
+    setCopyAniClass(style.copyAniShow)
     setTimeout(()=> {
       setCopyIconClass(style.icon)
-    }, 200)
-  }
-
-  const onRequireHide = () => {
-    setPartialHide(true)
+      setCopyAniClass(style.copyAniEnd)
+      setTimeout(() => {
+        setCopyAniClass((current) => {
+          if (current === style.copyAniEnd) {
+            return style.copyAniBegin
+          }
+          return current
+        })
+      }, 500)
+    }, 1000)
   }
 
   const showAllClick = () => {
     setPartialHide(false)
   }
 
+  useEffect(() => {
+    if (!codeContainer.current) {
+      return
+    }
+    if (codeContainer.current.clientHeight >= window.innerHeight / 2) {
+      setPartialHide(true)
+    }
+  }, [])
+
   return (
     <pre>
       <div className="z-50 relative" style={partialHide ? { maxHeight: '50vh' } : undefined}>
-        <RichContent {...props} wrapLine={wrapLineActive} onRequirePartialHide={onRequireHide} />
+        <div className={style.codeContent} 
+          ref={codeContainer} 
+          style={wrapLineActive ? { textWrap: 'wrap' } : undefined}
+          dangerouslySetInnerHTML={{ __html: props.content }}/>
         <div className={style.languageTag}>{props.lang}</div>
         <div className={style.toolBar}>
           <span title="自动换行">
@@ -74,8 +70,11 @@ const _PartialCodeBlock: React.FC<PartialCodeBlockProps> = props => {
               className={wrapLineActive ? style.iconClick : style.icon}><use
                 xlinkHref={Icons.WRAP_LINE}/></svg>
           </span>
-          <svg width={15} height={15} onMouseDown={onCopyDown} onMouseUp={onCopyUp} className={copyIconClass}><use
-            xlinkHref={Icons.COPY}/></svg>
+          <div className={style.copyContainer} title="复制">
+            <svg width={15} height={15} onMouseDown={onCopyDown} onMouseUp={onCopyUp} className={copyIconClass}><use
+              xlinkHref={Icons.COPY}/></svg>
+            <div className={concatClassName(style.copySuccessIcon, copyAniClass)}>复制成功</div>
+          </div>
         </div>
         {
           partialHide ?
