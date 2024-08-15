5cd78fdb7becc3d48ff08986ccb078e3601044f0
icebing <xu2237803016@outlook.com>
2024-08-15 23:47:25 +0800

    fix: 非安全域下无法复制代码

--- src/components/PartialCodeBlock/index.tsx
+++ src/components/PartialCodeBlock/index.tsx
@@ -20,21 +20,40 @@ const _PartialCodeBlock: React.FC<PartialCodeBlockProps> = props => {
   
 
   const onCopyUp = () => {
-    navigator.clipboard.writeText(props.content).catch(e => {
-      alert(e.message)
-    })
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(props.content).catch(e => {
+        alert(e.message)
+      })
+    } else {
+      const ctr = codeContainer.current
+      const selection = window.getSelection()
+      if (!ctr || !selection) {
+        alert('复制失败! 这可能是一个 BUG，如果可以，请提交一个 ISSUE 以帮助我们解决.')
+        return
+      }
+      const newRange = document.createRange()
+
+      newRange.setStart(ctr, 0)
+      newRange.setEnd(ctr, ctr.childNodes.length)
+
+      selection.removeAllRanges()
+      selection.addRange(newRange)
+      document.execCommand('copy')
+      console.warn('复制成功! 您的博客使用了非 https 链接，如果这是您的博客，请考虑切换到 https.')
+    }
   }
 
   const onExpandClick = () => {
     setExpandActive(!expandActive)
   }
 
+  const useFallbackCopy = !(navigator && navigator.clipboard)
 
   return (
     <pre>
       <div className="z-50 relative pt-12 pb-5">
         <div className={style.codeContent} 
-          ref={codeContainer} 
+          ref={useFallbackCopy ? codeContainer : undefined}
           style={{ textWrap: wrapLineActive ? 'wrap' : undefined, maxHeight: expandActive ? undefined : '50vh' }}
           dangerouslySetInnerHTML={{ __html: props.content }}/>
         <div className={style.languageTag}>{props.lang}</div>
