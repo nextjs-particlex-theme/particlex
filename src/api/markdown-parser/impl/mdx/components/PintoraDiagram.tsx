f527fd23a26fdb37aa15d704f146d68e449a6def
icebing <xu2237803016@outlook.com>
2024-09-13 17:27:10 +0800

    feat: mdx 提供流程图组件

---
+++ src/api/markdown-parser/impl/mdx/components/PintoraDiagram.tsx
@@ -0,0 +1,30 @@
+import type React from 'react'
+import type { PintoraConfig } from '@pintora/cli'
+import { render } from '@pintora/cli'
+
+
+interface PintoraDiagramProps {
+  code: string
+  config?: PintoraConfig
+}
+
+/**
+ * @see https://pintorajs.vercel.app/
+ */
+const PintoraDiagram: React.FC<PintoraDiagramProps> = async props => {
+  // const r = pintora.parseAndDraw(props.code, { config: props.config })
+  // console.log(r)
+  const svg = await render({
+    code: props.code,
+    pintoraConfig: props.config,
+    mimeType: 'image/svg+xml',
+    width: 1000,
+    backgroundColor: 'currentColor',
+    renderInSubprocess: false
+  })
+  return (
+    <div style={{ color: 'var(--color-card)', display: 'flex', justifyContent: 'center' }} dangerouslySetInnerHTML={{ __html: svg }}></div>
+  )
+}
+
+export default PintoraDiagram
