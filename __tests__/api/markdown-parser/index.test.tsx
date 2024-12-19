a785513a7f02ab15a71f84e21f318a13dc738dca
icebing <xu2237803016@outlook.com>
2024-12-19 15:22:59 +0800

    fix: 单元测试报错

--- __tests__/api/markdown-parser/index.test.tsx
+++ __tests__/api/markdown-parser/index.test.tsx
@@ -1,17 +1,17 @@
-// import { render } from '@testing-library/react'
-// import parseMarkdown, { splitMarkdownContent } from '@/api/markdown-parser'
-// import md from './markdown.test.md'
-// import mdx from './markdown.test.mdx'
-// import '@testing-library/jest-dom'
+import { render } from '@testing-library/react'
+import parseMarkdown from '@/api/markdown-parser'
+import md from './markdown.test.md'
+import mdx from './markdown.test.mdx'
+import '@testing-library/jest-dom'
 
-// test('Test md render', async () => {
-//   const parsed = await parseMarkdown(splitMarkdownContent(md).content, '.md')
-//   const rendered = render(parsed)
-//   expect(rendered.baseElement.innerHTML).toMatchSnapshot()
-// })
-//
-// test('Test mdx render', async () => {
-//   const parsed = await parseMarkdown(splitMarkdownContent(mdx as unknown as string).content, '.mdx')
-//   const rendered = render(parsed)
-//   expect(rendered.baseElement.innerHTML).toMatchSnapshot()
-// })
+test('Test md render', async () => {
+  const parsed = await parseMarkdown(md, '.md')
+  const rendered = render(parsed)
+  expect(rendered.baseElement.innerHTML).toMatchSnapshot()
+})
+
+test('Test mdx render', async () => {
+  const parsed = await parseMarkdown((mdx as unknown) as string, '.mdx')
+  const rendered = render(parsed)
+  expect(rendered.baseElement.innerHTML).toMatchSnapshot()
+})
